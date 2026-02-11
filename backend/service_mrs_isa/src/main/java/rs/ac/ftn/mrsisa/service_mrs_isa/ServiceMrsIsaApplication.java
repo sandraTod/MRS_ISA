253f21b0ba38ce70ffcedab7df2fbf19d21ddfb8
sandra_t <sakica995@gmail.com>
2026-02-11 10:18:50 +0100

    Podesavanje backend-a: domen + servisni moduli

---
+++ backend/service_mrs_isa/src/main/java/rs/ac/ftn/mrsisa/service_mrs_isa/ServiceMrsIsaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.mrsisa.service_mrs_isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMrsIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMrsIsaApplication.class, args);
+	}
+
+}
---
+++ backend/service_mrs_isa/src/test/java/rs/ac/ftn/mrsisa/service_mrs_isa/ServiceMrsIsaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.mrsisa.service_mrs_isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMrsIsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
